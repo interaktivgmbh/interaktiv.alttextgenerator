8abba755814a947c1c33cc95ef1d36b24f18b82c
Alexander Rybakov <rybakov@interaktiv.de>
2025-12-05 13:18:07 +0100

    this is not required

--- src/interaktiv/alttextgenerator/utils/generator.py
+++ src/interaktiv/alttextgenerator/utils/generator.py
@@ -2,12 +2,8 @@ from datetime import datetime
 from interaktiv.aiclient.client import AIClient
 from interaktiv.aiclient.helper import get_model_name_from_slug
 from interaktiv.aiclient.interfaces import IAIClient
-from interaktiv.alttextgenerator.behaviors.alt_text_metadata import (
-    IAltTextMetadataBehavior,
-)
 from interaktiv.alttextgenerator.helper import b64_resized_image
 from interaktiv.alttextgenerator.helper import construct_prompt_with_image
-from interaktiv.alttexts.behaviors.alt_text import IAltTextBehavior
 from plone.app.contenttypes.content import Image
 from plone.namedfile.file import NamedBlobImage
 from zope.component import getUtility
@@ -29,13 +25,10 @@ def generate_alt_text_suggestion(context: Image) -> bool:
 
     selected_model = get_model_name_from_slug(ai_client.selected_model, context)
 
-    alt_text_context = IAltTextBehavior(context)
-    alt_text_context.alt_text = alt_text
-
-    alt_text_metadata_context = IAltTextMetadataBehavior(context)
-    alt_text_metadata_context.alt_text_ai_generated = True
-    alt_text_metadata_context.alt_text_model_used = selected_model
-    alt_text_metadata_context.alt_text_generation_date = datetime.now().date()
+    context.alt_text = alt_text
+    context.alt_text_ai_generated = True
+    context.alt_text_model_used = selected_model
+    context.alt_text_generation_date = datetime.now().date()
 
     modified(context)
     context.reindexObject()
