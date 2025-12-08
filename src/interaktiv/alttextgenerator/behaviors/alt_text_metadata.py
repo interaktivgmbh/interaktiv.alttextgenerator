c4b34e3d9660586e828ff02cb19852ed7b0cbc22
Alexander Rybakov <rybakov@interaktiv.de>
2025-12-08 08:41:01 +0100

    fix order_after

--- src/interaktiv/alttextgenerator/behaviors/alt_text_metadata.py
+++ src/interaktiv/alttextgenerator/behaviors/alt_text_metadata.py
@@ -22,7 +22,7 @@ class IAltTextMetadataBehavior(model.Schema):
         required=False,
         default=False,
     )
-    directives.order_after("alt_text")
+    directives.order_after(alt_text_ai_generated="IAltTextBehavior.alt_text")
 
     alt_text_model_used = schema.TextLine(
         title=_("alt_text_model_used_label", default="Model used"),
