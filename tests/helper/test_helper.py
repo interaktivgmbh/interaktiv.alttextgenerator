12043696a1e15214411c0557943afcdbe8566991
Alexander Rybakov <rybakov@interaktiv.de>
2025-12-12 15:37:52 +0100

    added tests for helper.py

--- tests/helper/test_helper.py
+++ tests/helper/test_helper.py
@@ -1,4 +1,12 @@
-from interaktiv.alttextgenerator.helper import glob_matches
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from interaktiv.alttextgenerator.exc import ValidationError
+from interaktiv.alttextgenerator.helper import glob_matches, construct_prompt_from_context, check_whitelisted_mimetype, check_generation_allowed
+from plone.namedfile.file import NamedImage
+from Products.CMFPlone.tests import dummy
+
+import pytest
 
 
 class TestHelper:
@@ -59,3 +67,93 @@ class TestHelper:
         for test_case in test_cases:
             matches = glob_matches(test_case["glob"], test_case["path"])
             assert matches == test_case["match"]
+
+    def test_construct_prompt_from_context(self, portal):
+        # setup
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+        image = api.content.create(
+            type="Image",
+            id="test-image",
+            container=portal,
+            image=NamedImage(dummy.JpegImage(), "image/jpeg", "test.jpeg"),
+        )
+        api.portal.set_registry_record("interaktiv.alttextgenerator.user_prompt", "User Prompt")
+        api.portal.set_registry_record("interaktiv.alttextgenerator.system_prompt", "")
+
+        # do it
+        prompt = construct_prompt_from_context(image)
+
+        # post condition
+        # test that the system prompt is not included if no value is set
+        roles = {item["role"] for item in prompt}
+        assert "user" in roles
+        assert "system" not in roles
+
+        user_item = next(item for item in prompt if item["role"] == "user")
+        text_item = next(c for c in user_item["content"] if c["type"] == "text")
+        assert text_item["text"] == "User Prompt"
+
+        image_item = next(c for c in user_item["content"] if c["type"] == "image_url")
+        assert image_item["image_url"]["url"].startswith("data:image/")
+
+        # test that the system prompt is included if a value is set
+        api.portal.set_registry_record("interaktiv.alttextgenerator.system_prompt", "System Prompt")
+
+        prompt = construct_prompt_from_context(image)
+
+        roles = {item["role"] for item in prompt}
+        assert "user" in roles
+        assert "system" in roles
+
+        system_item = next(item for item in prompt if item["role"] == "system")
+        assert system_item["content"] == "System Prompt"
+
+        user_item = next(item for item in prompt if item["role"] == "user")
+        text_item = next(c for c in user_item["content"] if c["type"] == "text")
+        assert text_item["text"] == "User Prompt"
+
+        image_item = next(c for c in user_item["content"] if c["type"] == "image_url")
+        assert image_item["image_url"]["url"].startswith("data:image/")
+
+    def test_check_generation_allowed(self, portal):
+        # setup
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+        image = api.content.create(
+            type="Image",
+            id="test-image",
+            container=portal,
+            image=NamedImage(dummy.JpegImage(), "image/jpeg", "test.jpeg"),
+        )
+
+        # do it
+        check_generation_allowed(image)
+
+        # blacklist all items starting with "test-"
+        api.portal.set_registry_record("interaktiv.alttextgenerator.blacklisted_paths", ["test-*"])
+
+        with pytest.raises(ValidationError):
+            check_generation_allowed(image)
+
+
+    def test_check_whitelisted_mimetypes(self, portal):
+        # setup
+        setRoles(portal, TEST_USER_ID, ["Manager"])
+        evil_image = api.content.create(
+            type="Image",
+            id="evil-image",
+            container=portal,
+            image=NamedImage(dummy.JpegImage(), "image/evilImage", "evil-image.jpeg"),
+        )
+        good_image = api.content.create(
+            type="Image",
+            id="good-image",
+            container=portal,
+            image=NamedImage(dummy.JpegImage(), "image/jpeg", "good-image.jpeg"),
+        )
+
+        # do it
+        with pytest.raises(ValidationError):
+            check_whitelisted_mimetype(evil_image)
+
+        # this should not raise
+        check_whitelisted_mimetype(good_image)
